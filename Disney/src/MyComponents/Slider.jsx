bf23d984a84956ef8fb15f1da49a55913eb1ced3
PranavThorat1432 <pranavthorat95@gmail.com>
2025-05-28 20:39:06 +0530

    Add Slider.jsx

--- Disney/src/MyComponents/Slider.jsx
+++ Disney/src/MyComponents/Slider.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react'
 import GlobalAPI from '../Services/GlobalAPI'
+import { HiChevronRight, HiChevronLeft } from "react-icons/hi";
+
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original/';
+const screenWidth = window.innerWidth;
 
 function Slider() {
+    const elementRef = React.useRef(null);
     const [movieList, setMovieList] = React.useState([]);
     useEffect(() => {
         getTrendingMovies();
@@ -14,11 +18,26 @@ function Slider() {
             setMovieList(resp.data.results);
         });
     }
+
+    const sliderRight = (element) => {
+        element.scrollLeft += screenWidth > 768 ? 1350 : 600; 
+    }
+    const sliderLeft = (element) => {
+        element.scrollLeft -= screenWidth > 768 ? 1350 : 600; 
+    }
+
   return (
     <div>
-        {movieList.map((item, index) => {
-            <img key={index} src={IMAGE_BASE_URL + item.backdrop_path}/>
-        })}
+        <HiChevronLeft className='hidden md:block text-white text-[40px] absolute mx-8 mt-[150px] cursor-pointer' 
+        onClick={() => sliderLeft(elementRef.current)}/>
+        <HiChevronRight className='hidden md:block text-white text-[40px] absolute right-8 mt-[150px] cursor-pointer' 
+        onClick={() => sliderRight(elementRef.current)}/>
+
+        <div className='flex overflow-x-auto no-scrollbar gap-2 md:gap-5 p-5 w-full px-16 py-4 scroll-smooth' ref={elementRef}>  
+            {movieList.map((item, index) => (
+                <img key={index} src={IMAGE_BASE_URL + item.backdrop_path} className='min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] hover:border-gray-400 transition-all duration-100 ease-in-out'/>
+            ))}
+        </div>
     </div>
   )
 }
