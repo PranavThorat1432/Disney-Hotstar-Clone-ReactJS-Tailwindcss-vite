69de4a02235ccf4222ff5fc34267b1c91065c130
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-02 12:15:52 +0530

    Updates

--- Disney/src/MyComponents/MovieList.jsx
+++ Disney/src/MyComponents/MovieList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import GlobalAPI from '../Services/GlobalAPI'
+import GlobalAPI from '../Services/GlobalAPI.jsx'
 import MovieCard from './MovieCard.jsx'
 import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
 import HrMovieCard from './HrMovieCard.jsx';
@@ -30,15 +30,25 @@ function MovieList({genreID, index_}) {
 
     return (
         <div className="relative">
-            <IoChevronBackOutline onClick={() => slideLeft(elementRef.current)} className={`text-[50px] text-white p-2 z-10 cursor-pointer hidden md:block absolute left-0 top-1/2 -translate-y-1/2 ${index_%3==0 ? 'mt-0' : 'mt-0'}`}/>
+            <div className="flex items-center">
+                <IoChevronBackOutline
+                    onClick={() => slideLeft(elementRef.current)}
+                    className="text-[50px] text-white p-2 z-10 cursor-pointer hidden md:block"
+                />
 
-            <div ref={elementRef} className="flex overflow-x-auto gap-8 pt-5 px-3 pb-10 no-scrollbar scroll-smooth">
-                {MovieList.map((item, index) => (
-                    <MovieCard movie={item} key={index} />
-                ))}
+                <div ref={elementRef} className="flex overflow-x-auto gap-8 pt-5 px-3 pb-10 no-scrollbar scroll-smooth flex-1">
+                    {MovieList.map((item, index) => (
+                        <React.Fragment key={item.id || index}>
+                            {index_%3==0 ? <HrMovieCard movie={item}/> : <MovieCard movie={item} />}
+                        </React.Fragment>
+                    ))}
+                </div>
+
+                <IoChevronForwardOutline
+                    onClick={() => slideRight(elementRef.current)}
+                    className="text-[50px] text-white p-2 z-10 cursor-pointer hidden md:block"
+                />
             </div>
-
-            <IoChevronForwardOutline onClick={() => slideRight(elementRef.current)} className={`text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 absolute right-0 top-1/2 -translate-y-1/2 ${index_%3==0 ? 'mt-0' : 'mt-0'}`}/>
         </div>
     )
 }
