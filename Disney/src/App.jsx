866a8a3ce084815e44e5f15f064c7cce5cfb595a
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-05 13:39:32 +0530

    Update

--- Disney/src/App.jsx
+++ Disney/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './MyComponents/Header.jsx'
 import Slider from './MyComponents/Slider.jsx'
@@ -8,20 +5,13 @@ import ProductionHouse from './MyComponents/ProductionHouse.jsx'
 import GenreMovieList from './MyComponents/GenreMovieList.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className='min-h-screen'>
-        <Header/>
-
-        <Slider/>
-
-        <ProductionHouse/>
-
-        <GenreMovieList/>
-      </div>
-    </>
+    <div className='min-h-screen'>
+      <Header />
+      <Slider />
+      <ProductionHouse />
+      <GenreMovieList />
+    </div>
   )
 }
 
