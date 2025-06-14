7136ca133e8a8b7be91735479483ed71e0ca1adf
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-14 13:08:08 +0530

    Add React-router-dom

--- Disney/src/App.jsx
+++ Disney/src/App.jsx
@@ -1,17 +1,35 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './MyComponents/Header.jsx'
 import Slider from './MyComponents/Slider.jsx'
 import ProductionHouse from './MyComponents/ProductionHouse.jsx'
 import GenreMovieList from './MyComponents/GenreMovieList.jsx'
+import Search from './MyComponents/Search.jsx'
+import Favorites from './MyComponents/Favorites.jsx'
+import Watchlist from './MyComponents/Watchlist.jsx'
+import { MovieProvider } from './Context/MovieContext'
 
 function App() {
   return (
-    <div className='min-h-screen'>
-      <Header />
-      <Slider />
-      <ProductionHouse />
-      <GenreMovieList />
-    </div>
+    <Router>
+      <MovieProvider>
+        <div className='min-h-screen bg-gray-900'>
+          <Header />
+          <Routes>
+            <Route path="/search" element={<Search />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+            <Route path="/" element={
+              <>
+                <Slider />
+                <ProductionHouse />
+                <GenreMovieList />
+              </>
+            } />
+          </Routes>
+        </div>
+      </MovieProvider>
+    </Router>
   )
 }
 
